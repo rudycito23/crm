a38e3c88a3dad096ba0b9e59edbabbc3a94fce00
rudycito23 <rrlucas@live.com>
2023-06-24 09:34:37 -0500

    Created Login modal (to be continued); updated styling; added font file

---
+++ src/components/Modal.tsx
@@ -0,0 +1,48 @@
+export const Modal = () => {
+  return (
+    <>
+      <div id="modal-container" className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded py-1.5 px-8 mb-4">
+          <div>
+            <h1 className="font-bold my-3.5">Welcome back!</h1>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-grey-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username:
+            </label>
+            <input
+              required
+              id="username"
+              type="text"
+              placeholder="Username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-grey-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password:
+            </label>
+            <input
+              required
+              id="password"
+              type="text"
+              placeholder="********"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
